508b13ec4f8f052b58e13047b1cae3eac4fb6ec0
Austin Buhler <austinbuhler@gmail.com>
2020-04-14 20:12:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,6 +109,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+		programmer_hash[:alan_kay][:known_for] = "GUI"
+		programmer_hash
 
 end
 
@@ -132,6 +134,5 @@ def adding_to_dennis
       }
     }
 
-		programmer_hash[:alan_kay][:known_for] => "GUI"
-		programmer_hash
+
 end
